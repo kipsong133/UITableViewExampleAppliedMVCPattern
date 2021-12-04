f745ee226800b9863e074b49a159f1f5b42e98de
KimWooSun <kipsong133@gmail.com>
2021-12-04 17:48:20 +0900

    Initial Commit

---
+++ UITableViewExampleAppliedMVCPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewExampleAppliedMVCPattern
+//
+//  Created by 김우성 on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
